2a0ea698d4e8cd107446d737c485549e2a48a5f0
AhmadOthman <trollingosman101@outlook.com>
2024-05-17 02:02:12 +0300

    -  feat(css): add new styles for custom input label, text input, primary button, and loader -  feat(blade): add new blade components for custom input label, loader, primary button, text input -  feat(blade): create new blade layout main for registration page

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -9,8 +9,11 @@ use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
+new #[Layout('layouts.main')] class extends Component
 {
+
+    public string $firstName = '';
+    public string $lastName = '';
     public string $name = '';
     public string $email = '';
     public string $address = '';
@@ -20,8 +23,10 @@ new #[Layout('layouts.guest')] class extends Component
     /**
      * Handle an incoming registration request.
      */
-    public function register(): void
-    {
+    public function register(): void {
+
+        $this->name = trim($this->firstName) . ' ' . trim($this->lastName);
+
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
@@ -37,15 +42,122 @@ new #[Layout('layouts.guest')] class extends Component
 
         $this->redirect(RouteServiceProvider::HOME, navigate: true);
     }
-}; ?>
+}; 
+?>
 
-<div>
+<section class="bg-white dark:bg-gray-900">
+    <div class="flex justify-center min-h-screen">
+        <img class="hidden object-cover bg-cover lg:block lg:w-2/5 max-h-screen"
+            src="https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80">
+        </img>
+
+        <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+            <div class="w-full">
+                <h1 class="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+                    Get your free account now.
+                </h1>
+
+                <p class="mt-4 text-gray-500 dark:text-gray-400">
+                    Let’s get you all set up so you can verify your personal account and begin setting up your profile.
+                </p>
+
+                <form wire:submit="register" class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                    <div>
+                        <x-custom-input-label for="firstName" :value="__('FirstName')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="firstName" id="firstName" placeholder="John"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="text" name="firstName" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-custom-input-label for="lastName" :value="__('LastName')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="lastName" id="lastName" placeholder="Snow"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="text" name="lastName" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-custom-input-label for="address" :value="__('Address')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="address" id="address" placeholder="123 fake st."
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="text" name="address" required autocomplete="address" />
+                        <x-input-error :messages="$errors->get('address')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-custom-input-label for="email" :value="__('Email')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="email" id="email" placeholder="johnsnow@example.com"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="email" name="email" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-custom-input-label for="password" :value="__('Password')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="password" id="password" placeholder="Enter your password"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="password" name="password" required autofocus autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-custom-input-label for="password_confirmation" :value="__('Confirm password')"
+                            class="block mb-2 text-sm text-gray-600 dark:text-gray-200" />
+                        <x-text-input wire:model="password_confirmation" id="password_confirmation"
+                            placeholder="Enter your password"
+                            class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            type="password" name="password_confirmation" required autofocus
+                            autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex justify-between items-end">
+
+                        <p
+                            class="w-full text-sm text-gray-600 dark:text-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                            {{ __('Already registered?') }} <a href="{{ route('login') }}" wire:navigate
+                                class="underline hover:text-gray-900 dark:hover:text-gray-300 transition-colors duration-500">{{
+                                __(' Login') }}</a>
+                        </p>
+
+                    </div>
+                    <button type="submit"
+                        class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                        <span>{{ __('Register') }}</span>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+{{-- <div>
     <form wire:submit="register">
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required
-                autofocus autocomplete="name" />
+            <x-input-label for="firstName" :value="__('FirstName')" />
+            <x-text-input wire:model="firstName" id="firstName" class="block mt-1 w-full" type="text" name="firstName"
+                required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        <!-- Name -->
+        <div>
+            <x-input-label for="lastName" :value="__('LastName')" />
+            <x-text-input wire:model="lastName" id="lastName" class="block mt-1 w-full" type="text" name="lastName"
+                required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
@@ -96,4 +208,4 @@ new #[Layout('layouts.guest')] class extends Component
             </x-primary-button>
         </div>
     </form>
-</div>
+</div> --}}
