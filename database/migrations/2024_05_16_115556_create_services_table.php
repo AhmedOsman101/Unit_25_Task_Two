cc0ac8ea7bd984dd0bd20393d253ed346be1ad2a
AhmadOthman <trollingosman101@outlook.com>
2024-05-16 15:18:33 +0300

    -  feat(migrations): add 'address' field to 'users' table -  feat(migrations): create 'services' table with fields 'name', 'description', 'price', 'category_id' -  feat(migrations): create 'service_categories' table with field 'name'

--- database/migrations/2024_05_16_115556_create_services_table.php
+++ database/migrations/2024_05_16_115556_create_services_table.php
@@ -9,14 +9,12 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('address');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->unsignedDouble('price', places: 2);
+            $table->foreignId('category_id');
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration {
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 };
--- database/migrations/2024_05_16_115911_create_service_categories_table.php
+++ database/migrations/2024_05_16_115911_create_service_categories_table.php
@@ -9,14 +9,9 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('address');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -25,6 +20,6 @@ return new class extends Migration {
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
