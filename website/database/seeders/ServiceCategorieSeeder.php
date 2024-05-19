791999024de130e41820cd2f6112d4e446917708
AhmadOthman <trollingosman101@outlook.com>
2024-05-19 07:44:34 +0300

    Update ServiceCategorieSeeder.php

--- website/database/seeders/ServiceCategorieSeeder.php
+++ website/database/seeders/ServiceCategorieSeeder.php
@@ -2,14 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder {
     /**
      * Run the database seeds.
      */
     public function run(): void {
-        //
+        $categories = [
+            ['name' => 'Computer Repair'],
+            ['name' => 'Laptop Repair'],
+            ['name' => 'Data Recovery'],
+            ['name' => 'Virus Removal'],
+            ['name' => 'Network Setup'],
+            ['name' => 'Software Installation'],
+            ['name' => 'Hardware Upgrade'],
+            ['name' => 'Custom PC Build'],
+            ['name' => 'IT Consulting'],
+            ['name' => 'Remote Support']
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
