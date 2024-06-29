1042493dd149055bb565a75091cd9d1fd48d325e
AhmadOthman <trollingosman101@outlook.com>
2024-06-29 15:08:34 +0300

    update dashboard.blade.php

--- website/resources/views/dashboard.blade.php
+++ website/resources/views/dashboard.blade.php
@@ -2,14 +2,14 @@
     <div class="py-12 dark:text-gray-100">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @if ( count($requests) > 0 )
-                <div class="py-8">
-                    <h1 class="text-3xl font-bold pb-3">Requests</h1>
-                    @livewire('requests-table', ['requests' => $requests])
-                </div>
+            <div class="py-8">
+                <h1 class="text-3xl font-bold pb-3">Requests ({{count($requests)}})</h1>
+                @livewire('requests-table', ['requests' => $requests])
+            </div>
             @else
-                <div class="py-8">
-                    <x-no-requests/>
-                </div>
+            <div class="py-8">
+                <x-no-requests />
+            </div>
             @endif
         </div>
     </div>
